6e09aaa8ca288b5f567680a55138878b39f97e68
Ian Lum <ianlum314@gmail.com>
2024-01-22 15:55:04 -0500

    randomized bullets from left side of the screen

--- bullet_hell/bullet_hell_game.gd
+++ bullet_hell/bullet_hell_game.gd
@@ -4,14 +4,17 @@ extends Node2D
 @onready var timer: Timer = $Timer
 
 func _ready():
-	timer.wait_time = 0.5
+	timer.wait_time = 0.1
 
 func _process(delta):
 	pass
 
 func spawn_bullet():
 	var instance = bullet_scene.instantiate()
-	instance.start(Vector2(0,0), Vector2(1,1))
+	instance.start(
+		Vector2(0, randi_range(0, 1080)),
+		Vector2(1, randf_range(-1,1))
+	)
 	add_child(instance)
 
 func _on_timer_timeout():
