edc1826cc630bd8202e528048a24986e6e14dd1e
Ian Lum <ianlum314@gmail.com>
2024-01-22 10:10:34 -0500

    player detects bullets

--- bullet_hell/bh_player.gd
+++ bullet_hell/bh_player.gd
@@ -1,13 +1,19 @@
 extends CharacterBody2D
 
+@onready var hitbox: Area2D = $HitBox
+
 const SPEED = 300.0
 
-
 func _physics_process(delta):
-
 	var direction = Vector2(
 		Input.get_axis("left", "right"), Input.get_axis("up", "down")
 	).normalized()
 	velocity = direction * SPEED
-
+	
 	move_and_slide()
+
+func _on_hit_box_area_entered(area):
+	if area.is_in_group("bullet"):
+		modulate = Color(1,0,0)
+		await get_tree().create_timer(0.1).timeout
+		modulate = Color(1,1,1)
