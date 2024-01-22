6388ebdaba12e44e3d91831ca8439514fd8de979
mrSquidee <mr.squidy78@gmail.com>
2024-01-22 14:02:59 -0500

    fpm minimap movement

--- fp_maze/minimap.gd
+++ fp_maze/minimap.gd
@@ -2,7 +2,9 @@ extends Control
 
 @export var player : CharacterBody3D
 @onready var mini_player : Sprite2D = $Player
+@onready var map : TextureRect = $Minimap
 
 
 func _process(delta):
 	mini_player.rotation = -deg_to_rad(player.rotation_degrees.y)
+	mini_player.position = Vector2(player.position.x, player.position.z) + map.size/2
