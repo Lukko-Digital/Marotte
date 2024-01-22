e8267483915a8ac7bceeac65ef8b66522501c576
Ian Lum <ianlum314@gmail.com>
2024-01-22 15:48:03 -0500

    normalize direction var

--- bullet_hell/bullet.gd
+++ bullet_hell/bullet.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
-const SPEED = 400
+const SPEED = 10
 var direction: Vector2
 
 func start(start_pos: Vector2, dir: Vector2):
 	position = start_pos
-	direction = dir
+	direction = dir.normalized()
 	look_at(direction)
 
 func _ready():
