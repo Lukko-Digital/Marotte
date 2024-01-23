9c88d10a1ae45fb12e96680630b0f675fc0eddf0
Ian Lum <ianlum314@gmail.com>
2024-01-23 18:15:02 -0500

    add circle bullet effect

--- bullet_hell/bullet_hell_game.gd
+++ bullet_hell/bullet_hell_game.gd
@@ -3,19 +3,37 @@ extends Node2D
 @onready var bullet_scene = preload("res://bullet_hell/bullet.tscn")
 @onready var timer: Timer = $BulletTimer
 
+const LEFT_X = 758
+const RIGHT_X = 1625
+const TOP_Y = 73
+const BOTTOM_Y = 914
+
 func _ready():
-	timer.wait_time = 0.3
+	timer.wait_time = 2
 
 func _process(delta):
 	pass
 
-func spawn_bullet():
+func random_from_left():
 	var instance = bullet_scene.instantiate()
 	instance.start(
-		Vector2(758, randi_range(73, 914)),
-		Vector2(1, randf_range(-1,1))
+		Vector2(LEFT_X, randi_range(TOP_Y, BOTTOM_Y)),
+		Vector2(1, randf_range(0,1))
 	)
 	add_child(instance)
 
 func _on_timer_timeout():
-	spawn_bullet()
+#	random_from_left()
+	circle_from_left()
+
+func circle_from_left():
+	var num_shots = 12
+	var angle = PI/num_shots
+	var spawn_height = randi_range(TOP_Y, BOTTOM_Y)
+	for i in range(num_shots):
+		var instance = bullet_scene.instantiate()
+		instance.start(
+			Vector2(LEFT_X, spawn_height),
+			Vector2(0, -1).rotated(angle*i)
+		)
+		add_child(instance)
