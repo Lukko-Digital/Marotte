8df685a7c463426a19e47190131ee467ffd00f30
mrSquidee <mr.squidy78@gmail.com>
2024-01-22 20:55:01 -0500

    fpm enemy following

---
+++ fp_maze/enemy/fp_maze_enemy.gd
@@ -0,0 +1,30 @@
+extends CharacterBody3D
+
+@export var player : CharacterBody3D
+@onready var nav_agent : NavigationAgent3D = $NavigationAgent3D
+
+var SPEED = 3
+
+func _physics_process(delta):
+	
+	nav_agent.target_position = player.position
+	
+	var path = nav_agent.get_next_path_position()
+	
+	var step_size = delta * SPEED
+	var direction
+	
+	if path:
+		# Direction is the difference between where we are now
+		# and where we want to go.
+		direction = path - position
+
+		# Move the robot towards the path node, by how far we want to travel.
+		# Note: For a KinematicBody, we would instead use move_and_slide
+		# so collisions work properly.
+		velocity = direction.normalized() * SPEED
+		nav_agent.set_velocity_forced(velocity)
+		
+		print(velocity)
+
+	move_and_slide()
