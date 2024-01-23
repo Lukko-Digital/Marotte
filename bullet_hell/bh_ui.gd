6928db3e48f4fd2c2a259ef613afc251ada28aee
Ian Lum <ianlum314@gmail.com>
2024-01-23 11:47:49 -0500

    add pulse effect on low hp

--- bullet_hell/bh_ui.gd
+++ bullet_hell/bh_ui.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @onready var health_bar: ProgressBar = $HealthBar
+@onready var color_animation: AnimationPlayer = $ScreenColor/AnimationPlayer
 
 const MAX_HP = 10
 var current_hp = MAX_HP
@@ -9,10 +10,13 @@ func _ready():
 	health_bar.max_value = MAX_HP
 	health_bar.value = MAX_HP
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
+func _process(delta):
+	if current_hp <= MAX_HP/5:
+		color_animation.speed_scale = 2
+		color_animation.play("pulse")
+	elif current_hp <= MAX_HP/2:
+		color_animation.play("pulse")
 
 func _on_bh_player_hit():
 	current_hp -= 1
