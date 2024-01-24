e2c5c79dc3d7fa3dae21aed881130fde75c49cec
Ian Lum <ianlum314@gmail.com>
2024-01-24 17:22:56 -0500

    add variablility to camera shake, shake on wrong word

--- bullet_hell/display/bh_camera.gd
+++ bullet_hell/display/bh_camera.gd
@@ -1,22 +1,29 @@
 extends Camera2D
 
-const SHAKE_DURATION = 0.2
-const SHAKE_AMOUNT = 10
+var shake_amount
 
 @onready var timer: Timer = $ShakeTimer
 var default_offset: Vector2 = offset
 
 func _ready():
-	pass # Replace with function body.
+	Events.word_picked.connect(_on_word_picked)
 
 func _process(_delta):
 	offset = default_offset
 	handle_shake()
 
 func _on_bh_player_hit():
-	timer.start(SHAKE_DURATION)
+	shake(0.2, 10)
+
+func _on_word_picked(correct):
+	if not correct:
+		shake(0.2, 20)
 
 func handle_shake():
 	if not timer.is_stopped():
-		var shake_offset = Vector2(randf_range(-1, 1) * SHAKE_AMOUNT, randf_range(-1, 1) * SHAKE_AMOUNT)
+		var shake_offset = Vector2(randf_range(-1, 1) * shake_amount, randf_range(-1, 1) * shake_amount)
 		offset += shake_offset
+
+func shake(duration: float, amount: float):
+	timer.start(duration)
+	shake_amount = amount
