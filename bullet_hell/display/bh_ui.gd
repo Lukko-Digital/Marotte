99b93bf225fa08b3b8fadb083a67deeb30d47b1b
Ian Lum <ianlum314@gmail.com>
2024-01-24 17:32:16 -0500

    gain and lose hp on word pickup flash white on correct word use setter method for current hp

--- bullet_hell/display/bh_ui.gd
+++ bullet_hell/display/bh_ui.gd
@@ -6,9 +6,10 @@ extends Control
 
 
 const MAX_HP = 10
-var current_hp = MAX_HP
+var current_hp = MAX_HP : set = _set_current_hp
 
 func _ready():
+	Events.word_picked.connect(_on_word_picked)
 	health_bar.max_value = MAX_HP
 	health_bar.value = MAX_HP
 	player_image_animation.play("stress0")
@@ -26,7 +27,20 @@ func _process(_delta):
 		player_image_animation.play("stress1")
 
 
+func _set_current_hp(new_hp):
+	current_hp = clamp(new_hp, 0, MAX_HP)
+	health_bar.value = current_hp
+
+
 func _on_bh_player_hit():
 	current_hp -= 1
-	health_bar.value = current_hp
-	color_animation.play("flash")
+	color_animation.play("red_flash")
+
+
+func _on_word_picked(correct):
+	if correct:
+		current_hp += 2
+		color_animation.play("white_flash")
+	else:
+		current_hp -= 2
+		color_animation.play("red_flash")
