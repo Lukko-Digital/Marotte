57222ea5cfa52f9ca9ebbc376c18a55a86afa47a
Josh <joshuatehrlich@gmail.com>
2024-01-24 18:51:29 -0500

    hopefully this unfucks everything if not idefk

--- bullet_hell/display/bh_ui.gd
+++ bullet_hell/display/bh_ui.gd
@@ -6,13 +6,12 @@ extends Control
 
 
 const MAX_HP = 10
-var current_hp = MAX_HP
+var current_hp = MAX_HP : set = _set_current_hp
 
 func _ready():
+	Events.word_picked.connect(_on_word_picked)
 	health_bar.max_value = MAX_HP
 	health_bar.value = MAX_HP
-	player_image_animation.play("stress0")
-
 
 func _process(_delta):
 	if current_hp <= MAX_HP * 0.2:
@@ -24,9 +23,24 @@ func _process(_delta):
 		color_animation.play("pulse")
 	elif current_hp <= MAX_HP * 0.7:
 		player_image_animation.play("stress1")
+	else:
+		player_image_animation.play("stress0")
+
+
+func _set_current_hp(new_hp):
+	current_hp = clamp(new_hp, 0, MAX_HP)
+	health_bar.value = current_hp
 
 
 func _on_bh_player_hit():
 	current_hp -= 1
-	health_bar.value = current_hp
-	color_animation.play("flash")
+	color_animation.play("red_flash")
+
+
+func _on_word_picked(correct):
+	if correct:
+		current_hp += 2
+		color_animation.play("white_flash")
+	else:
+		current_hp -= 2
+		color_animation.play("red_flash")
