79dbe05654b9906dcd8bd61021c4ec3e99b5c963
Ian Lum <ianlum314@gmail.com>
2024-01-24 11:33:49 -0500

    play various stress animations

--- bullet_hell/bh_ui.gd
+++ bullet_hell/bh_ui.gd
@@ -2,10 +2,8 @@ extends Control
 
 @onready var health_bar: ProgressBar = $HealthBar
 @onready var color_animation: AnimationPlayer = $ScreenColor/AnimationPlayer
-@onready var player_image: TextureRect = $UIPlayer
+@onready var player_image_animation: AnimationPlayer = $UIPlayer/AnimationPlayer
 
-@export var low_stress: CompressedTexture2D
-@export var high_stress: CompressedTexture2D
 
 const MAX_HP = 10
 var current_hp = MAX_HP
@@ -13,16 +11,19 @@ var current_hp = MAX_HP
 func _ready():
 	health_bar.max_value = MAX_HP
 	health_bar.value = MAX_HP
-	player_image.texture = low_stress
+	player_image_animation.play("stress0")
 
 
 func _process(delta):
-	if current_hp <= MAX_HP/5:
+	if current_hp <= MAX_HP * 0.2:
+		player_image_animation.play("stress3")
 		color_animation.speed_scale = 2
 		color_animation.play("pulse")
-	elif current_hp <= MAX_HP/2:
+	elif current_hp <= MAX_HP * 0.5:
+		player_image_animation.play("stress2")
 		color_animation.play("pulse")
-		player_image.texture = high_stress
+	elif current_hp <= MAX_HP * 0.7:
+		player_image_animation.play("stress1")
 
 
 func _on_bh_player_hit():
