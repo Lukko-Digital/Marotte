1a5a2d14970f4b74912514e74d283f8d2e4ea549
Ian Lum <ianlum314@gmail.com>
2024-01-24 15:25:51 -0500

    organize and comment code

--- bullet_hell/bullet_hell_game.gd
+++ bullet_hell/bullet_hell_game.gd
@@ -6,16 +6,16 @@ extends Node2D
 @onready var timer: Timer = $BulletTimer
 @onready var player: CharacterBody2D = $bh_player
 
+## Wall Locations
 const LEFT_X = 758
 const RIGHT_X = 1625
 const TOP_Y = 73
 const BOTTOM_Y = 914
 
+## Spawn locations
 const PLAYER_SPAWN = Vector2(1200, 500)
-
 const WORD_SPAWN_X_OFFSET = 130
 const WORD_SPAWN_Y_OFFSET = 80
-
 const Word_Spawn = {
 	TOP_LEFT = Vector2(LEFT_X + WORD_SPAWN_X_OFFSET, TOP_Y + WORD_SPAWN_Y_OFFSET),
 	BOT_LEFT = Vector2(LEFT_X + WORD_SPAWN_X_OFFSET, BOTTOM_Y - WORD_SPAWN_Y_OFFSET),
@@ -23,6 +23,7 @@ const Word_Spawn = {
 	BOT_RIGHT = Vector2(RIGHT_X - WORD_SPAWN_X_OFFSET, BOTTOM_Y - WORD_SPAWN_Y_OFFSET)
 }
 
+## Direction enum
 const Direction = {
 	LEFT = Vector2(-1, 0),
 	RIGHT = Vector2(1, 0),
@@ -30,23 +31,34 @@ const Direction = {
 	DOWN = Vector2(0, 1)
 }
 
+
 func _ready():
 	timer.wait_time = 2
 	player.position = PLAYER_SPAWN
 	Events.correct_word_picked.connect(_reset)
 
-func _process(delta):
-	pass
 
+## Reset upon word pickup
 func _reset():
 	get_tree().call_group("bullet", "queue_free")
 	player.position = PLAYER_SPAWN
 	spawn(word_scene, Word_Spawn.values().pick_random(), Vector2())
 
+
+## Spawn external scene, used for bullets, warnings, and words
+func spawn(scene: PackedScene, position: Vector2, direction: Vector2):
+	var instance = scene.instantiate()
+	instance.start(position, direction)
+	add_child(instance)
+
+
+## Bullet Timer
 func _on_timer_timeout():
 	circle(Direction.values().pick_random(), 8)
 	circle(Direction.values().pick_random(), 8)
 
+
+## Circle shot
 func circle(direction: Vector2, num_shots=12):
 	var spawn_position: Vector2
 	var warning_dir = direction.rotated(PI)
@@ -71,15 +83,12 @@ func circle(direction: Vector2, num_shots=12):
 			shot_base_dir.rotated(PI/num_shots*i)
 		)
 
+
+## Random single shot
 func random_from_left():
 	var instance = bullet_scene.instantiate()
 	instance.start(
 		Vector2(LEFT_X, randi_range(TOP_Y, BOTTOM_Y)),
 		Vector2(1, randf_range(0,1))
 	)
-	add_child(instance)
-
-func spawn(scene: PackedScene, position: Vector2, direction: Vector2):
-	var instance = scene.instantiate()
-	instance.start(position, direction)
 	add_child(instance)
