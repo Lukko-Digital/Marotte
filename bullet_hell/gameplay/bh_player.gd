96818a1803ca12d4bc196350cc270c5953044789
Ian Lum <ianlum314@gmail.com>
2024-01-24 13:52:58 -0500

    implement left right run animation

--- bullet_hell/gameplay/bh_player.gd
+++ bullet_hell/gameplay/bh_player.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 
 @onready var hitbox: Area2D = $HitBox
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var animation_player: AnimationPlayer = $Sprite2D/AnimationPlayer
 
 const SPEED = 300.0
 
@@ -10,9 +12,23 @@ func _physics_process(delta):
 	var direction = Vector2(
 		Input.get_axis("left", "right"), Input.get_axis("up", "down")
 	).normalized()
+	handle_animation(direction)
 	velocity = direction * SPEED
 	
 	move_and_slide()
+
+func handle_animation(direction: Vector2):
+	if direction == Vector2():
+		# still
+		animation_player.play("idle")
+	elif direction.x > 0:
+		# right
+		animation_player.play("run_left")
+		sprite.flip_h = true
+	else:
+		# left
+		animation_player.play("run_left")
+		sprite.flip_h = false
 
 func _on_hit_box_area_entered(area):
 	if area.is_in_group("bullet"):
