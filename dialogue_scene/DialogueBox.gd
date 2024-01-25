2d4f2985fddd64201f1a92b168a30ac751aad432
mrSquidee <mr.squidy78@gmail.com>
2024-01-25 16:19:05 -0500

    dialogue scene animated text

--- dialogue_scene/DialogueBox.gd
+++ dialogue_scene/DialogueBox.gd
@@ -1,11 +1,39 @@
 extends TextureRect
 
 @export var character_name : String
+@export var dialogue_noise : AudioStreamPlayer
+
 @onready var label : Label = $Label
+@onready var text_timer : Timer = $TextTimer
+
+signal text_animation_done
+
+const TEXT_SPEED = 0.03
+
+const VOICE_PITCH_MIN: float = 0.9
+const VOICE_PITCH_MAX: float = 1.5
 
 func _on_dialogue_scene_active_speaker(speaker):
 	visible = speaker == character_name
 
 
 func _on_dialogue_scene_display_line(text):
-	label.text = text
+	if visible:
+		label.text = text
+		animate_display()
+
+
+
+func animate_display():
+	label.visible_characters = 0
+	while label.visible_characters < len(label.text):
+		dialogue_noise.pitch_scale = randf_range(VOICE_PITCH_MIN, VOICE_PITCH_MAX)
+		dialogue_noise.play()
+		label.visible_characters += 1
+		text_timer.start(TEXT_SPEED)
+		await text_timer.timeout
+	text_animation_done.emit()
+
+
+func _on_dialogue_scene_speed_animation():
+	label.visible_characters = len(label.text)
--- dialogue_scene/dialogue_scene.gd
+++ dialogue_scene/dialogue_scene.gd
@@ -2,20 +2,8 @@ extends CanvasLayer
 
 @export var script_file_path: String
 
-@onready var player_box: TextureRect = $PlayerDialogueBox
-@onready var player_label: Label = $PlayerDialogueBox/Label
-@onready var king_box: TextureRect = $KingDialogueBox
-@onready var king_label: Label = $KingDialogueBox/Label
-@onready var text_timer: Timer = $TextTimer
-@onready var dialogue_noise: AudioStreamPlayer = $DialogueAudioPlayer
-
 var active_label: Label
 var display_in_progress = false
-
-const TEXT_SPEED = 0.03
-
-const VOICE_PITCH_MIN: float = 0.9
-const VOICE_PITCH_MAX: float = 1.5
 
 var game_script: PackedStringArray
 var current_line: int = 0
@@ -25,6 +13,7 @@ var advance_on_keypress: bool = true
 
 signal active_speaker(speaker)
 signal display_line(text)
+signal speed_animation
 signal spawn_words(group)
 signal spawn_bullets(pattern)
 
@@ -42,23 +31,15 @@ func load_file():
 		FileAccess.READ
 	).get_as_text().strip_edges().split("\n")
 
-func animate_display():
-	active_label.visible_characters = 0
-	display_in_progress = true
-	while active_label.visible_characters < len(active_label.text):
-		dialogue_noise.pitch_scale = randf_range(VOICE_PITCH_MIN, VOICE_PITCH_MAX)
-		dialogue_noise.play()
-		active_label.visible_characters += 1
-		text_timer.start()
-		await text_timer.timeout
-	display_in_progress = false
-	
-
 func next_line():
-	current_line += 1
-	if current_line >= len(game_script):
-		return
-	parse_line(game_script[current_line])
+	if display_in_progress:
+		display_in_progress = false
+		speed_animation.emit()
+	else:
+		current_line += 1
+		if current_line >= len(game_script):
+			return
+		parse_line(game_script[current_line])
 
 
 func parse_line(line: String):
@@ -67,37 +48,16 @@ func parse_line(line: String):
 		"Player", "King":
 			active_speaker.emit(split[0])
 			display_line.emit(split[1])
+			display_in_progress = true
 			
 			var advance_condition = split[2]
 			if advance_condition == "keypress":
-#				timer.stop()
 				advance_on_keypress = true
-#			elif advance_condition.is_valid_int():
-#				timer.start(int(advance_condition))
 			else:
 				assert(false, "Error: Invalid advance condition")
 		_:
 			assert(false, "Error: Invalid tag in game script")
 
-#func advance_dialogue():
-#	if display_in_progress:
-#		# cut to the end of the line
-#		active_label.visible_characters = len(active_label.text)
-#	elif curr_dialogue_idx >= len(dialogue) - 1:
-#		# end of dialogue
-#		print("end")
-#	else:
-#		# next line
-#		curr_dialogue_idx += 1
-#		var line_split = dialogue[curr_dialogue_idx].split(": ")
-#		match line_split[0]:
-#			"Player":
-#				player_box.visible = true
-#				king_box.visible = false
-#				active_label = player_box.get_node("Label")
-#			"King":
-#				king_box.visible = true
-#				player_box.visible = false
-#				active_label = king_box.get_node("Label")
-#		active_label.text = line_split[1]
-#		await animate_display()
+
+func _on_dialogue_box_text_animation_done():
+	display_in_progress = false
