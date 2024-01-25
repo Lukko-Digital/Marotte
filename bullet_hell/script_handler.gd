d9ae28fad6afdfc65d117d7b0c2f7413ee816194
Ian Lum <ianlum314@gmail.com>
2024-01-24 21:40:37 -0500

    load in script, advance on timer

---
+++ bullet_hell/script_handler.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+var game_script: PackedStringArray
+var current_line: int = 0
+
+@onready var timer: Timer = $Timer
+
+signal active_speaker(speaker)
+signal display_line(text)
+
+func _ready():
+	load_file()
+	parse_line(game_script[0])
+
+
+func _process(_delta):
+	pass
+
+
+func load_file():
+	game_script = FileAccess.open(
+		get_parent().script_file_path,
+		FileAccess.READ
+	).get_as_text().strip_edges().split("\n")
+
+
+func next_line():
+	current_line += 1
+	parse_line(game_script[current_line])
+
+
+func parse_line(line: String):
+	var split = line.split(": ")
+	active_speaker.emit(split[0])
+	display_line.emit(split[1])
+	timer.start(int(split[2]))
+
+
+func _on_timer_timeout():
+	next_line()
