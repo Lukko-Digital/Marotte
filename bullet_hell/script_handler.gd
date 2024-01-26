e0427d809653f36d16285d8992a27014dd2ae371
Ian Lum <ianlum314@gmail.com>
2024-01-25 20:12:20 -0500

    only advance dialogue on correct word

--- bullet_hell/script_handler.gd
+++ bullet_hell/script_handler.gd
@@ -67,7 +67,7 @@ func _on_timer_timeout():
 
 
 func _on_word_picked(correct):
-	if advnace_condition == Advance_Conditions.PICKUP:
+	if advnace_condition == Advance_Conditions.PICKUP and correct:
 		next_line()
 
 
