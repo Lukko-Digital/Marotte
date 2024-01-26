6129722c686238af81f51263c34bcdad1cc9a8f6
Ian Lum <ianlum314@gmail.com>
2024-01-25 21:37:23 -0500

    fix thinking bar

--- bullet_hell/display/thinking_bar.gd
+++ bullet_hell/display/thinking_bar.gd
@@ -3,5 +3,8 @@ extends ProgressBar
 @onready var word_timer: Timer = get_parent()
 
 func _process(delta):
-	max_value = word_timer.wait_time
-	value = word_timer.time_left
+	if word_timer.is_stopped():
+		value = 0
+	else:
+		max_value = word_timer.wait_time
+		value = word_timer.wait_time - word_timer.time_left
