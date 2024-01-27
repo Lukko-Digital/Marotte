14ea3ef67ac9bc9bc366664437583b3e0aee6e5a
Ian Lum <ianlum314@gmail.com>
2024-01-27 14:34:31 -0500

    varying pitch ranges

--- dialogue_scene/DialogueAudioPlayer.gd
+++ dialogue_scene/DialogueAudioPlayer.gd
@@ -3,6 +3,14 @@ class_name DialogueAudioPlayer
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+const PITCH_RANGES = {
+	"Player" = [0.9, 1.1],
+	"King" = [0.9, 1.1],
+	"Jester" = [0.9, 1.1]
+}
+
 func play_sound(character: String):
 	animation_player.stop(true)
+	var pitch_range = PITCH_RANGES[character]
+	pitch_scale = randf_range(pitch_range[0], pitch_range[1])
 	animation_player.play(character)
