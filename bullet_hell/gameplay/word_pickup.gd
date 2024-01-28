017921468ec5a8218c07ff8f350e1f604956358f
Ian Lum <ianlum314@gmail.com>
2024-01-27 19:15:31 -0500

    red word pickups

--- bullet_hell/gameplay/word_pickup.gd
+++ bullet_hell/gameplay/word_pickup.gd
@@ -10,6 +10,7 @@ const HITBOX_HEIGHT = 25
 var correct
 var word
 var joke_text
+var red = false
 
 ## Expect args to contain two elements
 ## 1. A bool which indicates if the word is the correct word
@@ -19,12 +20,17 @@ var joke_text
 func start(start_pos: Vector2, args: Array):
 	position = start_pos
 	correct = args[0]
-	word = args[1]
+	if args[1][0] == "#":
+		word = args[1].substr(1)
+		red = true
+	else:
+		word = args[1]
 	joke_text = args[2]
 
 
 func _ready():
 	label.text = word
+	if red: label.add_theme_color_override("font_color", Color("ff6363"))
 	var rect = RectangleShape2D.new()
 	rect.size = Vector2(len(label.text) * CHAR_WIDTH, HITBOX_HEIGHT) * HITBOX_SCALE
 	hitbox.shape = rect
