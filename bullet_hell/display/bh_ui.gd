a54a5d3778a01e859f81bc857c8f08d6769cdd08
Ian Lum <ianlum314@gmail.com>
2024-02-04 17:08:50 -0500

    add are you sure to death

--- bullet_hell/display/bh_ui.gd
+++ bullet_hell/display/bh_ui.gd
@@ -17,7 +17,7 @@ extends Control
 @onready var text_timer: Timer = $ThoughtBubble/Label/TextTimer
 @onready var dialogue_audio: DialogueAudioPlayer = $ThoughtBubble/DialogueAudioPlayer
 @onready var death_text_label: Label = $Death/DeathText
-@onready var button_sound_animation: AnimationPlayer = $Death/ButtonSoundPlayer/AnimationPlayer
+@onready var button_sound_animation: AnimationPlayer = $Death/ButtonSoundAnimation
 
 const MAX_HP = 5
 var current_hp = MAX_HP : set = _set_current_hp
@@ -110,18 +110,11 @@ func _on_try_again_button_up():
 
 
 func _on_give_up_button_up():
-	get_tree().paused = false
-	get_tree().change_scene_to_file("res://bullet_hell/display/title_screne.tscn")
+	$Death/are_you_sure.start()
 
 
-func _on_button_hover():
-	button_sound_animation.stop(true)
-	button_sound_animation.play("hover")
-
-
-func _on_button_press():
-	button_sound_animation.stop(true)
-	button_sound_animation.play("click")
+func _on_are_you_sure_dont_quit():
+	$Death/TryAgain.grab_focus()
 
 
 func _set_current_hp(new_hp):
