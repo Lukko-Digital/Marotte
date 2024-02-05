ad1d5e530e45f7e31593b993ad04009df1563cdd
Ian Lum <ianlum314@gmail.com>
2024-02-04 19:37:02 -0500

    fix title screen buttons sounds work on focus no hover texture, hover grabs focus

--- bullet_hell/display/title_screne.gd
+++ bullet_hell/display/title_screne.gd
@@ -7,8 +7,12 @@ func _ready():
 	$NewGame.grab_focus()
 
 
-func _on_quit_pressed():
-	get_tree().quit()
+func _on_new_game_mouse_entered():
+	$NewGame.grab_focus()
+
+
+func _on_quit_mouse_entered():
+	$Quit.grab_focus()
 
 
 func _on_new_game_pressed():
@@ -16,6 +20,10 @@ func _on_new_game_pressed():
 	transition.play("end")
 	await transition.transition_finished
 	get_tree().change_scene_to_packed(difficulty_select)
+
+
+func _on_quit_pressed():
+	get_tree().quit()
 
 
 func _on_animation_player_animation_finished(anim_name):
